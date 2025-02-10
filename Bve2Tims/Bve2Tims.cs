c18b15bd366682ce3ce0048f8a4dd4e3a9587866
stop-pattern <40131396+stop-pattern@users.noreply.github.com>
2025-02-10 16:42:04 +0900

    add ref

--- Bve2Tims/Bve2Tims.cs
+++ Bve2Tims/Bve2Tims.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Forms;
 using BveEx.PluginHost.Plugins;
 using BveEx.PluginHost.Plugins.Extensions;
 
