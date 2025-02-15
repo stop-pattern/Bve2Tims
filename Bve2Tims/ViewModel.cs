5d8767e4d2eeca96f4caa2a1799b51eeb2519b14
stop-pattern <40131396+stop-pattern@users.noreply.github.com>
2025-02-16 02:08:51 +0900

    add interface impl

--- Bve2Tims/ViewModel.cs
+++ Bve2Tims/ViewModel.cs
@@ -26,126 +26,211 @@ namespace Bve2Tims
         public string Unit0
         {
             get { return Model.UnitIndexes.ElementAtOrDefault(0).ToString(); }
-            set { Model.UnitIndexes[0] = int.Parse(value); }
+            set
+            {
+                Model.UnitIndexes[0] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
 
         public string Unit1
         {
             get { return Model.UnitIndexes.ElementAtOrDefault(1).ToString(); }
-            set { Model.UnitIndexes[1] = int.Parse(value); }
+            set
+            {
+                Model.UnitIndexes[1] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
 
         public string Unit2
         {
             get { return Model.UnitIndexes.ElementAtOrDefault(2).ToString(); }
-            set { Model.UnitIndexes[2] = int.Parse(value); }
+            set
+            {
+                Model.UnitIndexes[2] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
 
         public string Unit3
         {
             get { return Model.UnitIndexes.ElementAtOrDefault(3).ToString(); }
-            set { Model.UnitIndexes[3] = int.Parse(value); }
+            set
+            {
+                Model.UnitIndexes[3] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
+
         public string Unit4
         {
             get { return Model.UnitIndexes.ElementAtOrDefault(4).ToString(); }
-            set { Model.UnitIndexes[4] = int.Parse(value); }
+            set
+            {
+                Model.UnitIndexes[4] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
 
         public string Door0
         {
             get { return Model.DoorIndexes.ElementAtOrDefault(0).ToString(); }
-            set { Model.DoorIndexes[0] = int.Parse(value); }
+            set
+            {
+                Model.DoorIndexes[0] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
 
         public string Door1
         {
             get { return Model.DoorIndexes.ElementAtOrDefault(1).ToString(); }
-            set { Model.DoorIndexes[1] = int.Parse(value); }
+            set
+            {
+                Model.DoorIndexes[1] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
 
         public string Door2
         {
             get { return Model.DoorIndexes.ElementAtOrDefault(2).ToString(); }
-            set { Model.DoorIndexes[2] = int.Parse(value); }
+            set
+            {
+                Model.DoorIndexes[2] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
 
         public string Door3
         {
             get { return Model.DoorIndexes.ElementAtOrDefault(3).ToString(); }
-            set { Model.DoorIndexes[3] = int.Parse(value); }
+            set
+            {
+                Model.DoorIndexes[3] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
 
         public string Door4
         {
             get { return Model.DoorIndexes.ElementAtOrDefault(4).ToString(); }
-            set { Model.DoorIndexes[4] = int.Parse(value); }
+            set
+            {
+                Model.DoorIndexes[4] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
 
         public string Door5
         {
             get { return Model.DoorIndexes.ElementAtOrDefault(5).ToString(); }
-            set { Model.DoorIndexes[5] = int.Parse(value); }
+            set
+            {
+                Model.DoorIndexes[5] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
 
         public string Door6
         {
             get { return Model.DoorIndexes.ElementAtOrDefault(6).ToString(); }
-            set { Model.DoorIndexes[6] = int.Parse(value); }
+            set
+            {
+                Model.DoorIndexes[6] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
 
         public string Door7
         {
             get { return Model.DoorIndexes.ElementAtOrDefault(7).ToString(); }
-            set { Model.DoorIndexes[7] = int.Parse(value); }
+            set
+            {
+                Model.DoorIndexes[7] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
 
         public string Door8
         {
             get { return Model.DoorIndexes.ElementAtOrDefault(8).ToString(); }
-            set { Model.DoorIndexes[8] = int.Parse(value); }
+            set
+            {
+                Model.DoorIndexes[8] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
 
         public string Door9
         {
             get { return Model.DoorIndexes.ElementAtOrDefault(9).ToString(); }
-            set { Model.DoorIndexes[9] = int.Parse(value); }
+            set
+            {
+                Model.DoorIndexes[9] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
 
         public string Door10
         {
             get { return Model.DoorIndexes.ElementAtOrDefault(10).ToString(); }
-            set { Model.DoorIndexes[10] = int.Parse(value); }
+            set
+            {
+                Model.DoorIndexes[10] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
 
         public string Door11
         {
             get { return Model.DoorIndexes.ElementAtOrDefault(11).ToString(); }
-            set { Model.DoorIndexes[11] = int.Parse(value); }
+            set
+            {
+                Model.DoorIndexes[11] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
 
         public string Door12
         {
             get { return Model.DoorIndexes.ElementAtOrDefault(12).ToString(); }
-            set { Model.DoorIndexes[12] = int.Parse(value); }
+            set
+            {
+                Model.DoorIndexes[12] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
 
         public string Door13
         {
             get { return Model.DoorIndexes.ElementAtOrDefault(13).ToString(); }
-            set { Model.DoorIndexes[13] = int.Parse(value); }
+            set
+            {
+                Model.DoorIndexes[13] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
 
         public string Door14
         {
             get { return Model.DoorIndexes.ElementAtOrDefault(14).ToString(); }
-            set { Model.DoorIndexes[14] = int.Parse(value); }
+            set
+            {
+                Model.DoorIndexes[14] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
 
         public string Door15
         {
             get { return Model.DoorIndexes.ElementAtOrDefault(15).ToString(); }
-            set { Model.DoorIndexes[15] = int.Parse(value); }
+            set
+            {
+                Model.DoorIndexes[15] = int.Parse(value);
+                NotifyPropertyChanged();
+            }
         }
 
         #endregion
@@ -158,7 +243,11 @@ namespace Bve2Tims
         public bool AutoStart
         {
             get { return model.AutoStart; }
-            set { model.AutoStart = value; }
+            set
+            {
+                model.AutoStart = value;
+                NotifyPropertyChanged();
+            }
         }
 
         /// <summary>
@@ -175,7 +264,11 @@ namespace Bve2Tims
         public int SelectedDestinationIndex
         {
             get { return model.SelectedDestinationIndex; }
-            set { model.SelectedDestinationIndex = value; }
+            set
+            {
+                model.SelectedDestinationIndex = value;
+                NotifyPropertyChanged();
+            }
         }
 
         /// <summary>
@@ -184,7 +277,11 @@ namespace Bve2Tims
         public Udp SelectedDestination
         {
             get { return model.SelectedDestination; }
-            set { model.SelectedDestination = value; }
+            set
+            {
+                model.SelectedDestination = value;
+                NotifyPropertyChanged();
+            }
         }
 
         /// <summary>
@@ -193,7 +290,11 @@ namespace Bve2Tims
         public ObservableCollection<Udp> Destinations
         {
             get { return new ObservableCollection<Udp>(model.Destinations); }
-            set { model.Destinations = new List<Udp>(value); }
+            set
+            {
+                model.Destinations = new List<Udp>(value);
+                NotifyPropertyChanged();
+            }
         }
 
         #endregion
