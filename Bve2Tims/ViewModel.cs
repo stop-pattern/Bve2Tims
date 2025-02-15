49360112f6423cfc828cc6ad8a7138824b2db0cb
stop-pattern <40131396+stop-pattern@users.noreply.github.com>
2025-02-16 01:58:27 +0900

    add include

--- Bve2Tims/ViewModel.cs
+++ Bve2Tims/ViewModel.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Bve2Tims
 {
